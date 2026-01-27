5799418d2906b72ec7cdcf0c235a0580139d793f
Alex Ferrier <a.ferrier@ucl.ac.uk>
2026-01-27 12:39:40 +0000

    Update DDBH_Nsite_PP_runscript.sh

--- DDBH_Nsite_PP_template/DDBH_Nsite_PP_runscript.sh
+++ DDBH_Nsite_PP_template/DDBH_Nsite_PP_runscript.sh
@@ -4,10 +4,10 @@
 
 # Parameters; gamma = 1 taken as energy unit by default.  
 N=3; # Number of sites 
-Delta=0.1; # On-site energy detuning
+Delta=0.0; # On-site energy detuning
 Uint=0.1; # Two-particle interaction strength
 FPmp=1.0; # Coherent drive amplitude
-Jhop=0.45; # Hopping strength
+Jhop=3.0; # Hopping strength
 time_run=1000; # end time of simulation
 
 for s in {1..1000} # Loop over 1 to total number of stochastic trajectories
